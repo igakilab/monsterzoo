8bb99d3a1584c37d21acae8d93ec49371fd16568
tomato <nomu.tomato.zyusu@gmail.com>
2020-10-07 06:59:44 +0900

    メソッドかできるものはメセッッド化をした(途中)

--- src/Egg.java
+++ src/Egg.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+
 public class Egg {
     public Boolean isBone;
     public Double distance;
     public Egg(){
         this.isBone = false;
         this.distance = 0.0;
+    }
+    public Monster boneMonster(ArrayList<Monster> mondex){
+        if(this.distance>=3.0){
+            this.isBone=true;
+			System.out.println("卵が孵った！");
+			int m = (int)(mondex.size()*Math.random());
+			System.out.println(mondex.get(m).monsterName+"が産まれた！");
+            this.distance=0.0;
+            return mondex.get(m);
+        } else {
+            return null;
+        }
     }
 }
--- src/MonsterZoo.java
+++ src/MonsterZoo.java
@@ -23,51 +23,50 @@ public class MonsterZoo {
 
 		int flg1 = (int)(Math.random()*10);//0,1の場合はズーstation，7~9の場合はモンスター
 		if(flg1<=1){
-			System.out.println("ズーstationを見つけた！");
-			int b=(int)(Math.random()*3);//ball,fruits,eggがランダムに出る
-			int f=(int)(Math.random()*2);
-			int e=(int)(Math.random()*2);
-			System.out.println("ボールを"+b+"個，"+"フルーツを"+f+"個"+"卵を"+e+"個Getした！");
-			this.balls=this.balls+b;
-			this.fruits=this.fruits+f;
-			if(e>=1){//卵を1つ以上Getしたら
-				//egg[]に10個以上卵がない場合は新しい卵データをセットする
-				if(userHavingEggs.stream().filter(i->!i.isBone).count()<10)
-					this.userHavingEggs.add(new Egg());
-			}
+			this.arriveAtZooStation();
 		}else if(flg1>=7){
-			int m = (int)(this.mondex.size()*Math.random());//monsterZukanからランダムにモンスターを出す
-			System.out.println(this.mondex.get(m).monsterName+"が現れた！");
-			for(int i=0;i<3&&this.balls>0;i++){//捕まえる or 3回ボールを投げるまで繰り返す
-				int r = (int)(6*Math.random());//0~5までの数字をランダムに返す
-				if(this.fruits>0){
-					System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
-					this.fruits--;
-					r = r * 2;
-				}
-				System.out.println(this.mondex.get(m).monsterName+"にボールを投げた");
-				this.balls--;
-				if(this.mondex.get(m).monsterRate<=r){//monsterRare[m]の値がr以下の場合
-					System.out.println(this.mondex.get(m).monsterName+"を捕まえた！");
-					this.userGetedMonster.add(this.mondex.get(m));
-					break;//ボール投げ終了
-				}else{
-					System.out.println(this.mondex.get(m).monsterName+"に逃げられた！");
-				}
-			}
+			this.encountMonster();
 		}
 		this.userHavingEggs.stream().filter(i->(!i.isBone&&i.distance>=3.0)).forEach(i->{
-			i.isBone=true;
-			System.out.println("卵が孵った！");
-			/*モンスーター誕生 */
-			int m = (int)(this.mondex.size()*Math.random());
-			System.out.println(this.mondex.get(m).monsterName+"が産まれた！");
-			this.userGetedMonster.add(this.mondex.get(m));
-			i.distance=0.0;
+			this.userGetedMonster.add(i.boneMonster(this.mondex));
 		});
 
 	}
-
+	public void arriveAtZooStation(){
+		System.out.println("ズーstationを見つけた！");
+		int b=(int)(Math.random()*3);//ball,fruits,eggがランダムに出る
+		int f=(int)(Math.random()*2);
+		int e=(int)(Math.random()*2);
+		System.out.println("ボールを"+b+"個，"+"フルーツを"+f+"個"+"卵を"+e+"個Getした！");
+		this.balls=this.balls+b;
+		this.fruits=this.fruits+f;
+		if(e>=1){//卵を1つ以上Getしたら
+			//egg[]に10個以上卵がない場合は新しい卵データをセットする
+			if(userHavingEggs.stream().filter(i->!i.isBone).count()<10)
+				this.userHavingEggs.add(new Egg());
+		}
+	}
+	public void encountMonster(){
+		int m = (int)(this.mondex.size()*Math.random());//monsterZukanからランダムにモンスターを出す
+		System.out.println(this.mondex.get(m).monsterName+"が現れた！");
+		for(int i=0;i<3&&this.balls>0;i++){//捕まえる or 3回ボールを投げるまで繰り返す
+			int r = (int)(6*Math.random());//0~5までの数字をランダムに返す
+			if(this.fruits>0){
+				System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
+				this.fruits--;
+				r = r * 2;
+			}
+			System.out.println(this.mondex.get(m).monsterName+"にボールを投げた");
+			this.balls--;
+			if(this.mondex.get(m).monsterRate<=r){//monsterRare[m]の値がr以下の場合
+				System.out.println(this.mondex.get(m).monsterName+"を捕まえた！");
+				this.userGetedMonster.add(this.mondex.get(m));
+				break;//ボール投げ終了
+			}else{
+					System.out.println(this.mondex.get(m).monsterName+"に逃げられた！");
+				}
+			}
+	}
 	public double getDistance() {
 		return distance;
 	}
