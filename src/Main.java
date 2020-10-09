b0fac36dbb2b07d9d2a0a5cf5121d235724f6dd6
OKA <g1644222@cc.kyoto-su.ac.jp>
2020-10-09 19:55:34 +0900

    move()内の処理をモンスタバトル,消費アイテム,モンスタの誕生に分け,配列をlist形式に変更した.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 public class Main {
 	static MonsterZoo pz = new MonsterZoo();
@@ -35,33 +35,35 @@ public class Main {
 
 	//テスト用のモンスターデータを登録するメソッド
 	public static void setMonsterZukan(){
-		String tempMonster[] = new String[22];
-		double tempMonsterRare[] = new double[22];
-		tempMonster[0]="イガキン";	tempMonsterRare[0]=9;
-		tempMonster[1]="ナマチュウ";	tempMonsterRare[1]=3;
-		tempMonster[2]="イノウエン";	tempMonsterRare[2]=1;
-		tempMonster[3]="リョージィ";	tempMonsterRare[3]=2;
-		tempMonster[4]="アキモトン";	tempMonsterRare[4]=5;
-		tempMonster[5]="ゴージマ";	tempMonsterRare[5]=4;
-		tempMonster[6]="チュウデン";	tempMonsterRare[6]=6;
-		tempMonster[7]="ヅカホン";	tempMonsterRare[7]=8;
-		tempMonster[8]="ニシムラー";	tempMonsterRare[8]=7;
-		tempMonster[9]="サコーデン";	tempMonsterRare[9]=2;
-		tempMonster[10]="ウッチー";	tempMonsterRare[10]=5;
-		tempMonster[11]="ハヤッシー";	tempMonsterRare[11]=4;
-		tempMonster[12]="キーチー";	tempMonsterRare[12]=3;
-		tempMonster[13]="リョクン";	tempMonsterRare[13]=1;
-		tempMonster[14]="デコポン";	tempMonsterRare[14]=6;
-		tempMonster[15]="カミサン";	tempMonsterRare[15]=5;
-		tempMonster[16]="シスイ";	tempMonsterRare[16]=1;
-		tempMonster[17]="ジョナ";	tempMonsterRare[17]=7;
-		tempMonster[18]="ギダギダ";	tempMonsterRare[18]=2;
-		tempMonster[19]="ミッツー";	tempMonsterRare[19]=8;
-		tempMonster[20]="ゾエサン";	tempMonsterRare[20]=5;
-		tempMonster[21]="キタバー";	tempMonsterRare[21]=3;
+		// String tempMonster[] = new String[22];
+		// double monster_rare[] = new double[22];
+		ArrayList<String> monster_type = new ArrayList<String>();
+		ArrayList<Integer> monster_rare = new ArrayList<Integer>();
+		monster_type.add("イガキン");	monster_rare.add(9);
+		monster_type.add("ナマチュウ");	monster_rare.add(3);
+		monster_type.add("イノウエン");	monster_rare.add(1);
+		monster_type.add("リョージィ");	monster_rare.add(2);
+		monster_type.add("アキモトン");	monster_rare.add(5);
+		monster_type.add("ゴージマ");	monster_rare.add(4);
+		monster_type.add("チュウデン");	monster_rare.add(6);
+		monster_type.add("ヅカホン");	monster_rare.add(8);
+		monster_type.add("ニシムラー");	monster_rare.add(7);
+		monster_type.add("サコーデン");	monster_rare.add(2);
+		monster_type.add("ウッチー");	monster_rare.add(5);
+		monster_type.add("ハヤッシー");	monster_rare.add(4);
+		monster_type.add("キーチー");	monster_rare.add(3);
+		monster_type.add("リョクン");	monster_rare.add(1);
+		monster_type.add("デコポン");	monster_rare.add(6);
+		monster_type.add("カミサン");	monster_rare.add(5);
+		monster_type.add("シスイ");	monster_rare.add(1);
+		monster_type.add("ジョナ");	monster_rare.add(7);
+		monster_type.add("ギダギダ");	monster_rare.add(2);
+		monster_type.add("ミッツー");	monster_rare.add(8);
+		monster_type.add("ゾエサン");	monster_rare.add(5);
+		monster_type.add("キタバー");	monster_rare.add(3);
 
-		pz.setMonsterZukan(tempMonster);
-		pz.setMonsterRare(tempMonsterRare);
+		pz.setMonsterZukan(monster_type);
+		pz.setMonsterRare(monster_rare);
 	}
 
 }
--- src/MonsterZoo.java
+++ src/MonsterZoo.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 public class MonsterZoo {
 	double distance=0.0;//歩いた距離
@@ -16,8 +16,10 @@ public class MonsterZoo {
 
 	//モンスター図鑑．モンスターの名前とレア度(0.0~9.0)がそれぞれの配列に保存されている
 	//レア度が高いほうが捕まえにくい
-	String monsterZukan[] = new String[22];
-	double monsterRare[] = new double[22];
+	// String monsterZukan[] = new String[22];
+	// double monsterRare[] = new double[22];
+	ArrayList<String> monsterZukan = new ArrayList<String>();
+	ArrayList<Integer> monsterRare = new ArrayList<Integer>();
 
 	//呼び出すと1km distanceが増える
 	void move(){
@@ -30,58 +32,70 @@ public class MonsterZoo {
 
 		int flg1 = (int)(Math.random()*10);//0,1の場合はズーstation，7~9の場合はモンスター
 		if(flg1<=1){
-			System.out.println("ズーstationを見つけた！");
-			int b=(int)(Math.random()*3);//ball,fruits,eggがランダムに出る
-			int f=(int)(Math.random()*2);
-			int e=(int)(Math.random()*2);
-			System.out.println("ボールを"+b+"個，"+"フルーツを"+f+"個"+"卵を"+e+"個Getした！");
-			this.balls=this.balls+b;
-			this.fruits=this.fruits+f;
-			if(e>=1){//卵を1つ以上Getしたら
-				//egg[]に10個以上卵がない場合は新しい卵データをセットする
-				for(int i=0;i<this.eggDistance.length;i++){
-					if(this.egg[i]==false){
-						this.egg[i]=true;
-						this.eggDistance[i]=0.0;
-						break;
-					}
-				}
-			}
+			ball_and_fruit_and_egg_get();
 		}else if(flg1>=7){
-			int m = (int)(this.monsterZukan.length*Math.random());//monsterZukanからランダムにモンスターを出す
-			System.out.println(this.monsterZukan[m]+"が現れた！");
-			for(int i=0;i<3&&this.balls>0;i++){//捕まえる or 3回ボールを投げるまで繰り返す
-				int r = (int)(6*Math.random());//0~5までの数字をランダムに返す
-				if(this.fruits>0){
-					System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
-					this.fruits--;
-					r = r * 2;
-				}
-				System.out.println(this.monsterZukan[m]+"にボールを投げた");
-				this.balls--;
-				if(this.monsterRare[m]<=r){//monsterRare[m]の値がr以下の場合
-					System.out.println(this.monsterZukan[m]+"を捕まえた！");
-					for(int j=0;j<userMonster.length;j++){
-						if(this.userMonster[j]==null){
-							this.userMonster[j]=this.monsterZukan[m];
-							break;
-						}
-					}
-					break;//ボール投げ終了
-				}else{
-					System.out.println(this.monsterZukan[m]+"に逃げられた！");
+			monster_meet_and_battle();
+		}
+		monster_bone();
+	}
+
+	void ball_and_fruit_and_egg_get(){
+		System.out.println("ズーstationを見つけた！");
+		int b=(int)(Math.random()*3);//ball,fruits,eggがランダムに出る
+		int f=(int)(Math.random()*2);
+		int e=(int)(Math.random()*2);
+		System.out.println("ボールを"+b+"個，"+"フルーツを"+f+"個"+"卵を"+e+"個Getした！");
+		this.balls=this.balls+b;
+		this.fruits=this.fruits+f;
+		if(e>=1){//卵を1つ以上Getしたら
+			//egg[]に10個以上卵がない場合は新しい卵データをセットする
+			for(int i=0;i<this.eggDistance.length;i++){
+				if(this.egg[i]==false){
+					this.egg[i]=true;
+					this.eggDistance[i]=0.0;
+					break;
 				}
 			}
 		}
+	}
+
+	void monster_meet_and_battle(){
+		int m = (int)(this.monsterZukan.size()*Math.random());//monsterZukanからランダムにモンスターを出す
+		System.out.println(this.monsterZukan.get(m)+"が現れた！");
+		for(int i=0;i<3&&this.balls>0;i++){//捕まえる or 3回ボールを投げるまで繰り返す
+			int r = (int)(6*Math.random());//0~5までの数字をランダムに返す
+			if(this.fruits>0){
+				System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
+				this.fruits--;
+				r = r * 2;
+			}
+			System.out.println(this.monsterZukan.get(m)+"にボールを投げた");
+			this.balls--;
+			if(this.monsterRare.get(m)<=r){//monsterRare[m]の値がr以下の場合
+				System.out.println(this.monsterZukan.get(m)+"を捕まえた！");
+				for(int j=0;j<userMonster.length;j++){
+					if(this.userMonster[j]==null){
+						this.userMonster[j]=this.monsterZukan.get(m);
+						break;
+					}
+				}
+				break;//ボール投げ終了
+			}else{
+				System.out.println(this.monsterZukan.get(m)+"に逃げられた！");
+			}
+		}
+	}
+
+	void monster_bone(){
 		for(int i=0;i<this.egg.length;i++){
 			if(this.egg[i]==true&&this.eggDistance[i]>=3){
 				System.out.println("卵が孵った！");
-				int m = (int)(this.monsterZukan.length*Math.random());
-				System.out.println(this.monsterZukan[m]+"が産まれた！");
+				int m = (int)(this.monsterZukan.size()*Math.random());
+				System.out.println(this.monsterZukan.get(m)+"が産まれた！");
 
 				for(int j=0;j<userMonster.length;j++){
 					if(this.userMonster[j]==null){
-						this.userMonster[j]=this.monsterZukan[m];
+						this.userMonster[j]=this.monsterZukan.get(m); //ここをappendに変更する
 						break;
 					}
 				}
@@ -107,11 +121,11 @@ public class MonsterZoo {
 		return userMonster;
 	}
 
-	public void setMonsterZukan(String[] monsterZukan) {
+	public void setMonsterZukan(ArrayList<String> monsterZukan) {
 		this.monsterZukan = monsterZukan;
 	}
 
-	public void setMonsterRare(double[] monsterRare) {
+	public void setMonsterRare(ArrayList<Integer> monsterRare) {
 		this.monsterRare = monsterRare;
 	}
 }
